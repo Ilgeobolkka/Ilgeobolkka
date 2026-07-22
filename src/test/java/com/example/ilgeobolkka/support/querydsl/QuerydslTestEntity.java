79962a7afe62e24818fc150b6cb9fcf237a2580d
0younge <s45628439@gmail.com>
2026-07-22 19:46:50 +0900

    test: SCRUM-91 QueryDSL 리뷰 검증 보강
    운영용 annotation processor 설정 누락과 Hibernate·MySQL 런타임 호환성 회귀를 테스트에서 검출할 수 있게 한다.

--- src/test/java/com/example/ilgeobolkka/support/querydsl/QuerydslTestEntity.java
+++ src/test/java/com/example/ilgeobolkka/support/querydsl/QuerydslTestEntity.java
@@ -2,9 +2,11 @@ package com.example.ilgeobolkka.support.querydsl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
+@Table(name = "querydsl_test_entity")
 class QuerydslTestEntity {
 
     @Id
